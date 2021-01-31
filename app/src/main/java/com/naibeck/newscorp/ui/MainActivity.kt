c1e2e59d645fda6c90efd2b0c5445c4c60ffbcdd
Naibeck <kg-diseno@outlook.es>
2021-01-31 14:05:39 -0300

    Launch transition from fragment when item is clicked

--- app/src/main/java/com/naibeck/newscorp/ui/MainActivity.kt
+++ app/src/main/java/com/naibeck/newscorp/ui/MainActivity.kt
@@ -1,25 +1,14 @@
 package com.naibeck.newscorp.ui
 
-import android.content.Intent
 import android.os.Bundle
 import android.view.View
-import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.app.ActivityOptionsCompat
-import androidx.core.view.ViewCompat
 import androidx.databinding.DataBindingUtil
-import androidx.recyclerview.widget.GridLayoutManager
-import androidx.recyclerview.widget.RecyclerView.Adapter.StateRestorationPolicy
 import com.naibeck.newscorp.R
-import com.naibeck.newscorp.data.network.dto.PlaceholderImageItem
 import com.naibeck.newscorp.databinding.ActivityMainBinding
 import com.naibeck.newscorp.presentation.ContainerView
-import com.naibeck.newscorp.presentation.ImagesAdapter
-import com.naibeck.newscorp.presentation.ImagesView
-import com.naibeck.newscorp.presentation.PlaceholderImagesPresenter
 import com.naibeck.newscorp.presentation.extension.hide
 import com.naibeck.newscorp.presentation.extension.show
-import com.naibeck.newscorp.runtime.getApp
 
 class MainActivity : AppCompatActivity(), ContainerView {
     private var binding: ActivityMainBinding? = null
@@ -28,8 +17,6 @@ class MainActivity : AppCompatActivity(), ContainerView {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
         loadFragment()
-        //loadImages()
-        //setupRefresh()
     }
 
     private fun loadFragment() {
@@ -38,9 +25,6 @@ class MainActivity : AppCompatActivity(), ContainerView {
             .commit()
     }
 
-//    override val presenter: PlaceholderImagesPresenter
-//        get() = PlaceholderImagesPresenter(view = this, runtime = getApp().runtimeContext)
-
     override fun showProgress() {
         binding?.progress?.show()
     }
@@ -48,15 +32,6 @@ class MainActivity : AppCompatActivity(), ContainerView {
     override fun hideProgress() {
         binding?.progress?.hide()
     }
-
-//    override fun show(images: List<PlaceholderImageItem>) {
-//        setupImages(adapter = ImagesAdapter(
-//            placeholderImages = images,
-//            imagesView = this
-//        )
-//        )
-//        binding?.refresh?.isRefreshing = false
-//    }
 
 //    override fun onImageClick(imageView: ImageView?, url: String) {
 //        imageView?.let {
@@ -71,21 +46,6 @@ class MainActivity : AppCompatActivity(), ContainerView {
     override fun showError() {
         binding?.errorView?.container?.visibility = View.VISIBLE
     }
-
-//    private fun setupImages(adapter: ImagesAdapter) {
-//        adapter.stateRestorationPolicy = StateRestorationPolicy.PREVENT_WHEN_EMPTY
-//        binding?.images?.layoutManager = GridLayoutManager(this, resources.getInteger(R.integer.images_span))
-//        binding?.images?.adapter = adapter
-//    }
-
-//    private fun setupRefresh() {
-//        binding?.refresh?.setOnRefreshListener {
-//            binding?.refresh?.isRefreshing = true
-//            loadImages()
-//        }
-//    }
-//
-//    private fun loadImages() = presenter.loadImages()
 //
 //    private fun generateTransitionOption(imageView: ImageView): ActivityOptionsCompat =
 //        ActivityOptionsCompat.makeSceneTransitionAnimation(
--- app/src/main/java/com/naibeck/newscorp/ui/PlaceholderFragment.kt
+++ app/src/main/java/com/naibeck/newscorp/ui/PlaceholderFragment.kt
@@ -1,22 +1,29 @@
 package com.naibeck.newscorp.ui
 
+import android.app.Activity
 import android.content.Context
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
-import androidx.constraintlayout.widget.Placeholder
+import androidx.core.app.ActivityOptionsCompat
+import androidx.core.view.ViewCompat
 import androidx.databinding.DataBindingUtil
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.naibeck.newscorp.R
 import com.naibeck.newscorp.data.network.dto.PlaceholderImageItem
 import com.naibeck.newscorp.databinding.FragmentPlaceholderBinding
 import com.naibeck.newscorp.presentation.ContainerView
+import com.naibeck.newscorp.presentation.ImagesAdapter
 import com.naibeck.newscorp.presentation.ImagesView
 import com.naibeck.newscorp.presentation.PlaceholderImagesPresenter
 import com.naibeck.newscorp.runtime.getApp
-import timber.log.Timber
+import com.naibeck.newscorp.ui.MainActivity.Companion.IMAGE_NAME
+import com.naibeck.newscorp.ui.MainActivity.Companion.IMAGE_URL
 
 class PlaceholderFragment : Fragment(), ImagesView {
 
@@ -33,33 +40,40 @@ class PlaceholderFragment : Fragment(), ImagesView {
     ): View? {
 
         binding = DataBindingUtil.inflate(inflater, R.layout.fragment_placeholder, container, false)
+        setupRefresh()
         return binding?.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        presenter.loadImages()
+        loadImages()
     }
 
-    override fun showProgress() {
-        containerView.showProgress()
-    }
+    override fun showProgress() = containerView.showProgress()
 
-    override fun hideProgress() {
-        containerView.hideProgress()
-    }
+    override fun hideProgress() = containerView.hideProgress()
 
     override fun show(images: List<PlaceholderImageItem>) {
-        Timber.d(images.toString())
+        setupImages(
+            adapter = ImagesAdapter(
+                placeholderImages = images,
+                imagesView = this
+            )
+        )
+        binding?.refresh?.isRefreshing = false
     }
 
     override fun onImageClick(imageView: ImageView?, url: String) {
+        imageView?.let {
+            val intent = Intent(context, PlaceholderImageDetailActivity::class.java)
+            intent.putExtra(IMAGE_URL, url)
+            intent.putExtra(IMAGE_NAME, ViewCompat.getTransitionName(imageView))
 
+            startActivity(intent, generateTransitionOption(imageView).toBundle())
+        }
     }
 
-    override fun showError() {
-        containerView.showError()
-    }
+    override fun showError() = containerView.showError()
 
     override fun onAttach(context: Context) {
         super.onAttach(context)
@@ -67,6 +81,30 @@ class PlaceholderFragment : Fragment(), ImagesView {
             is ContainerView -> containerView = context
         }
     }
+
+    private fun loadImages() = presenter.loadImages()
+
+    private fun setupImages(adapter: ImagesAdapter) {
+        adapter.stateRestorationPolicy =
+            RecyclerView.Adapter.StateRestorationPolicy.PREVENT_WHEN_EMPTY
+        binding?.images?.layoutManager =
+            GridLayoutManager(context, resources.getInteger(R.integer.images_span))
+        binding?.images?.adapter = adapter
+    }
+
+    private fun setupRefresh() {
+        binding?.refresh?.setOnRefreshListener {
+            binding?.refresh?.isRefreshing = true
+            loadImages()
+        }
+    }
+
+    private fun generateTransitionOption(imageView: ImageView): ActivityOptionsCompat =
+        ActivityOptionsCompat.makeSceneTransitionAnimation(
+            containerView as Activity,
+            imageView,
+            ViewCompat.getTransitionName(imageView) ?: "imageView"
+        )
 
     companion object {
         fun getInstance() = PlaceholderFragment()
